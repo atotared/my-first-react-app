ee1ad255406df88910e51082cf81b964a55e70c8
atotared <atota@red.software.systems>
2021-02-01 22:08:11 +0100

    Add remaining props to Comment component

--- src/Comment.js
+++ src/Comment.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import faker from 'faker';
+
 
 const Comment = (props) => {
     console.log(props);
     return (
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.image()}></img>
+                    <img alt="avatar" src={props.avatar}></img>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
                         {props.author}
                     </a>
                     <div className="metadata">
-                        <span className="date">Posted on {faker.date.weekday()}</span>
+                        <span className="date">Posted {props.timeAgo}</span>
                     </div>
-                    <div className="text">{faker.lorem.sentence()}</div>
+                    <div className="text">{props.commentText}</div>
                 </div>
             </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Comment from './Comment';
+import faker from 'faker';
 
 const App = () => {
     return(
         <>
             <div className="ui container comments">
-            <Comment author="Paul"/>
-            <Comment author="John"/>
-            <Comment author="Ringo"/>
+            <Comment author="Paul" timeAgo="Yesterday at 4:00 PM" commentText="Nice blog post!" avatar={faker.image.image()}/>
+            <Comment author="John" timeAgo="Today at 10:00 AM" commentText="Interesting post!"  avatar={faker.image.image()} />
+            <Comment author="Ringo" timeAgo="Two days ago at 13:30 PM" commentText="Very useful post."  avatar={faker.image.image()} />
             </div>
         </>
     )
